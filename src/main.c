922135ff3b59bd73486da25617d7425f3ac14024
Dorian <dorian.bourgeoisat@icloud.com>
2020-07-09 01:33:02 +0200

    make sure that stack is in page

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 uint8_t kernel_stack[0x1000] __attribute__((section(".stack"),used)) = {0};
 struct stivale_header stivalehd __attribute__((section(".stivalehdr"),used)) = {
-    .stack = (uintptr_t)&kernel_stack + sizeof(kernel_stack),
+    .stack = (uintptr_t)&kernel_stack + sizeof(kernel_stack) - 1,
     .flags = 0x0001
 };
 
