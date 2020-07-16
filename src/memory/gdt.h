d48560bf4e93d3b974add99d854b4855563f1d43
Dorian <dorian.bourgeoisat@icloud.com>
2020-07-16 13:25:24 +0200

    added gdtd

--- src/memory/gdt.h
+++ src/memory/gdt.h
@@ -20,24 +20,6 @@ struct GDTE gdt[] = {
         0, // limit using pages
         0  // base 3
     },
-    { // kernel data segment
-        0xFFFF, // limit 1
-        0, // base 1
-        0, // base 2
-        0, // accessed
-        1, // read_write
-        0, // direction conforming
-        0, // code
-        1, // descriptor
-        0, // ring
-        1, // present
-        0xF, // limit2
-        0, // nullbits
-        1, // longmode
-        1, // 32 bit protected
-        1, // limit using pages
-        0  // base 3
-    },
     { // kernel code segment
         0xFFFF, // limit 1
         0, // base 1
@@ -56,7 +38,7 @@ struct GDTE gdt[] = {
         1, // limit using pages
         0  // base 3
     },
-    { // user data segment
+    { // kernel data segment
         0xFFFF, // limit 1
         0, // base 1
         0, // base 2
@@ -65,7 +47,7 @@ struct GDTE gdt[] = {
         0, // direction conforming
         0, // code
         1, // descriptor
-        3, // ring
+        0, // ring
         1, // present
         0xF, // limit2
         0, // nullbits
@@ -82,6 +64,24 @@ struct GDTE gdt[] = {
         0, // read_write can't write to code
         0, // direction conforming
         1, // code
+        1, // descriptor
+        3, // ring
+        1, // present
+        0xF, // limit2
+        0, // nullbits
+        1, // longmode
+        1, // 32 bit protected
+        1, // limit using pages
+        0  // base 3
+    },
+    { // user data segment
+        0xFFFF, // limit 1
+        0, // base 1
+        0, // base 2
+        0, // accessed
+        1, // read_write
+        0, // direction conforming
+        0, // code
         1, // descriptor
         3, // ring
         1, // present
@@ -127,7 +127,12 @@ struct GDTE gdt[] = {
         0, // 32 bit protected
         0, // limit using pages
         0  // base 3
-    },
+    }
+};
+
+struct GDTD gdtd = {
+    sizeof(gdt)-1,
+    (uintptr_t)gdt
 };
 
 struct GDTE_TSS gdte_tss = {
