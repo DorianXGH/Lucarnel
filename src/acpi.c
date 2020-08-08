f40b5b2c9e557c515e7bbca0c9cc4555f2c7fc46
Dorian <dorian.bourgeoisat@icloud.com>
2020-08-08 17:20:02 +0200

    formatting

--- src/acpi.c
+++ src/acpi.c
@@ -2,12 +2,10 @@
 
 bool do_checksum(struct ACPISDTHeader* table_header)
 {
-    unsigned char sum = 0;
- 
+    uint8_t sum = 0;
     for (int i = 0; i < table_header->length; i++)
     {
-        sum += ((char *) table_header)[i];
+        sum += ((uint8_t*)table_header)[i];
     }
- 
     return sum == 0;
 }
