91456c5560b285ef570a557f768fd1f4a30ba05f
Dorian <dorian.bourgeoisat@icloud.com>
2020-08-11 14:42:51 +0200

    finished adding APIC

--- src/includes/apic.h
+++ src/includes/apic.h
@@ -26,4 +26,21 @@ struct APICConfig
     struct APICRegister in_service[8];
     struct APICRegister trigger_mode[8];
     struct APICRegister interrupt_request[8];
+    struct APICRegister error_status;
+    struct APICRegister reserved3[6];
+    struct APICRegister LVT_corrected_machine_check_interrupt;
+    struct APICRegister interrupt_command[2];
+    struct APICRegister LVT_timer;
+    struct APICRegister LVT_thermal_sensor;
+    struct APICRegister LVT_performance_monitoring_counters;
+    struct APICRegister LVT_LINT0;
+    struct APICRegister LVT_LINT1;
+    struct APICRegister LVT_error;
+    struct APICRegister timer_initial_count;
+    struct APICRegister timer_current_count;
+    struct APICRegister reserved4[4];
+    struct APICRegister timer_divide_configuration;
+    struct APICRegister reserved5;
+
 };
+STATIC_ASSERT(sizeof(struct APICConfig) == 0x400, wrong_APIC_config_size);
